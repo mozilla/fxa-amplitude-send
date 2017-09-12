7e5219240130bc67c9fd21f0fc0022f7e1a51f9e
Phil Booth <pmbooth@gmail.com>
2017-09-12 08:56:10 +0100

    feat(fn): basic sending with no error handling

--- amplitude.py
+++ amplitude.py
@@ -1,20 +1,35 @@
+import hashlib
+import hmac
 import json
+import os
+import requests
 import sys
+
+API_KEY = os.environ["FXA_AMPLITUDE_API_KEY"]
+HMAC_KEY = os.environ["FXA_AMPLITUDE_HMAC_KEY"]
 
 def transform(line):
     try:
-        parsed_line = json.loads(line)
-
-        assert(type(parsed_line) is dict)
-
-        event = parsed_line["Fields"]
+        event = json.loads(line)["Fields"]
 
         assert("time" in event)
         assert("user_id" in event or "device_id" in event)
+
+        if "user_id" in event:
+            assert(event["user_id"] == event["user_properties"]["fxa_uid"])
+
+            user_id_hmac = hmac.new(HMAC_KEY, event["user_id"], hashlib.sha256)
+            event["user_id"] = user_id_hmac.hexdigest()
+            event["user_properties"]["fxa_uid"] = event["user_id"]
+
+        response = requests.post("https://api.amplitude.com/httpapi",
+                                 data={"api_key": API_KEY, "event": json.dumps((event))})
+
+        return response
 
     except:
         return None
 
 if __name__ == "__main__":
-    transform(sys.argv[1])
+    print transform(sys.argv[1])
 
