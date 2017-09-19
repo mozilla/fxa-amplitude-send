91501be33bc4e56ee0cf7fea50a1d15b2860ed8f
Phil Booth <pmbooth@gmail.com>
2017-09-19 09:42:12 +0100

    fix(fn): ditch threading timer in favour of time.sleep

--- amplitude.py
+++ amplitude.py
@@ -5,8 +5,7 @@ import json
 import os
 import requests
 import sys
-
-from threading import Timer
+import time
 
 AMPLITUDE_API_KEY = os.environ["FXA_AMPLITUDE_API_KEY"]
 HMAC_KEY = os.environ["FXA_AMPLITUDE_HMAC_KEY"]
@@ -14,7 +13,7 @@ HMAC_KEY = os.environ["FXA_AMPLITUDE_HMAC_KEY"]
 # For crude pre-emptive rate-limit obedience.
 MAX_EVENTS_PER_BATCH = 10
 MAX_BATCHES_PER_SECOND = 100
-BATCH_INTERVAL = 1.0 / MAX_BATCHES_PER_SECOND
+MIN_BATCH_INTERVAL = 1.0 / MAX_BATCHES_PER_SECOND
 
 def handle (message):
     # http://docs.aws.amazon.com/AmazonS3/latest/dev/notification-content-structure.html
@@ -29,13 +28,13 @@ def handle (message):
     s3 = boto3.resource("s3", region_name=record["awsRegion"])
     s3_object = s3.Object(record["s3"]["bucket"]["name"], record["s3"]["object"]["key"])
 
-    send(s3_object.get()["Body"].read().decode("utf-8").splitlines())
+    send(s3_object.get()["Body"].read().decode("utf-8"))
 
-def send (events):
+def process (events):
     batch = []
 
-    while len(batch) < MAX_EVENTS_PER_BATCH and len(events) > 0:
-        event = json.loads(events.pop())
+    for event_string in events.splitlines():
+        event = json.loads(event_string)
 
         # https://amplitude.zendesk.com/hc/en-us/articles/204771828#keys-for-the-event-argument
         assert("device_id" in event or "user_id" in event)
@@ -60,6 +59,16 @@ def send (events):
         event["insert_id"] = insert_id_hmac.hexdigest()
 
         batch.append(event)
+        if len(batch) == MAX_EVENTS_PER_BATCH:
+            send(batch)
+
+    if len(batch) > 0:
+        send(batch)
+
+def send (batch):
+    batch_interval = time.time() - send.batch_time
+    if batch_interval < MIN_BATCH_INTERVAL:
+        time.sleep(MIN_BATCH_INTERVAL - batch_interval)
 
     # https://amplitude.zendesk.com/hc/en-us/articles/204771828#request-format
     response = requests.post("https://api.amplitude.com/httpapi",
@@ -70,10 +79,10 @@ def send (events):
         # one failed request fails an entire dump from S3.
         raise RuntimeError("HTTP {} response")
 
-    if len(events) > 0:
-        # Wait 10 milliseconds before starting the next batch
-        Timer(BATCH_INTERVAL, send, [events]).start()
+    send.batch_time = time.time()
+
+send.batch_time = 0
 
 if  __name__ == "__main__":
-    send(sys.argv[1].splitlines())
+    process(sys.argv[1])
 
