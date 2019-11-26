6098a16a6c4bf50330eee9e18caf493df55c0747
Jon Buckley <jbuckley@mozilla.com>
2019-11-26 03:31:37 -0500

    a jbuck special

--- pubsub/index.js
+++ pubsub/index.js
@@ -6,6 +6,7 @@
 
 const async = require('async')
 const crypto = require('crypto')
+const equal = require('fast-deep-equal');
 const is = require('check-types')
 const logger = require('pino')({
   // https://cloud.google.com/logging/docs/agent/configuration#timestamp-processing
@@ -14,248 +15,152 @@ const logger = require('pino')({
 const { lookup } = require('lookup-dns-cache')
 const { PubSub } = require('@google-cloud/pubsub')
 const request = require('request-promise')
+const retry = require('async-retry')
 
-const { AMPLITUDE_API_KEY, HMAC_KEY, PUBSUB_PROJECT, PUBSUB_TOPIC, PUBSUB_SUBSCRIPTION } = process.env
+const { AMPLITUDE_API_KEY, HMAC_KEY, MAX_EVENTS_PER_BATCH, PUBSUB_PROJECT, PUBSUB_TOPIC, PUBSUB_SUBSCRIPTION } = process.env
 
-if (! AMPLITUDE_API_KEY || ! HMAC_KEY || ! PUBSUB_PROJECT || ! PUBSUB_TOPIC || ! PUBSUB_SUBSCRIPTION) {
+if (! AMPLITUDE_API_KEY || ! HMAC_KEY || ! PUBSUB_PROJECT || ! PUBSUB_SUBSCRIPTION || ! PUBSUB_TOPIC) {
   logger.fatal({type: 'startup.error'}, 'Error: You must set AMPLITUDE_API_KEY, HMAC_KEY, PUBSUB_PROJECT, PUBSUB_TOPIC and PUBSUB_SUBSCRIPTION environment variables')
   process.exit(1)
 }
-
-const SECOND = 1000;
-const MINUTE = SECOND * 60;
-const HOUR = MINUTE * 60;
-const DAY = HOUR * 24;
-
-// If TIMEOUT_THRESHOLD milliseconds pass with no messages arriving, the script will abort
-const TIMEOUT_THRESHOLD = parseInt(process.env.TIMEOUT_THRESHOLD || MINUTE);
-
-// If a message older than WARNING_THRESHOLD milliseconds arrives, the script will log a warning
-const WARNING_THRESHOLD = parseInt(process.env.WARNING_THRESHOLD || DAY * 3);
-
-const IGNORED_EVENTS = new Map()
-if (process.env.IGNORED_EVENTS) {
-  // process.env.IGNORED_EVENTS is a JSON object of event_type:criteria, e.g.:
-  //
-  // {
-  //   "fxa_activity - access_token_checked": [
-  //     {
-  //       "event_properties": {
-  //         "oauth_client_id": "deadbeef"
-  //       }
-  //     },
-  //     {
-  //       "event_properties": {
-  //         "oauth_client_id": "baadf00d"
-  //       }
-  //     }
-  //   ],
-  //   "fxa_activity - access_token_created": [
-  //     {
-  //       "event_properties": {
-  //         "oauth_client_id": "deadbeef"
-  //       }
-  //     },
-  //     {
-  //       "event_properties": {
-  //         "oauth_client_id": "baadf00d"
-  //       }
-  //     }
-  //   ]
-  // }
-  Object.entries(JSON.parse(process.env.IGNORED_EVENTS)).forEach(([ type, criteria ]) => {
-    IGNORED_EVENTS.set(type, criteria)
-  })
-}
-
-const ENDPOINTS = {
-  HTTP_API: 'https://api.amplitude.com/httpapi',
-  IDENTIFY_API: 'https://api.amplitude.com/identify',
-}
-
-const KEYS = {
-  HTTP_API: 'event',
-  IDENTIFY_API: 'identification',
-}
-
-const IDENTIFY_VERBS = [ '$set', '$setOnce', '$add', '$append', '$unset' ]
-const IDENTIFY_VERBS_SET = new Set(IDENTIFY_VERBS)
-const MAX_EVENTS_PER_BATCH = 10
-const WORKER_COUNT = process.env.WORKER_COUNT ? parseInt(process.env.WORKER_COUNT) : 1
-const MESSAGES = new Map()
-
-main()
-  .catch(error => {
-    logger.fatal({ type: 'main.error', error }, 'Main function error')
-    process.exit(1)
-  })
 
 async function main () {
   const pubsub = new PubSub({
     projectId: PUBSUB_PROJECT
   })
-
   const topic = pubsub.topic(PUBSUB_TOPIC)
-  const subscraption = topic.subscription(PUBSUB_SUBSCRIPTION)
-  const [ exists ] = await subscraption.exists()
-
-  const [ subscription ] = await (exists ? subscraption.get(PUBSUB_SUBSCRIPTION) : subscraption.create(PUBSUB_SUBSCRIPTION))
-  subscription.setOptions({
-    ackDeadline: 60,
+  const subscription = topic.subscription(PUBSUB_SUBSCRIPTION, {
+    ackDeadline: 300,
     flowControl: {
       maxExtension: 5 * 60
     }
   })
 
-  const cargo = {
-    httpapi: setupCargo(ENDPOINTS.HTTP_API, KEYS.HTTP_API),
-    identify: setupCargo(ENDPOINTS.IDENTIFY_API, KEYS.IDENTIFY_API),
-  }
-
-  let timeout;
-
   subscription.on('message', message => {
-    if (timeout) {
-      clearTimeout(timeout);
+    cargo.push(message)
+  })
+
+  const cargo = async.cargo(async (payload) => {
+    let identify_events = []
+    let http_events = payload.map(message => {
+      let event = JSON.parse(Buffer.from(message.data, 'base64').toString()).jsonPayload
+
+      if (event.Fields) {
+        event = event.Fields
+    
+        if (event.op && event.data) {
+          event = JSON.parse(event.data)
+        } else {
+          if (is.nonEmptyString(event.event_properties)) {
+            event.event_properties = JSON.parse(event.event_properties)
+          }
+    
+          if (is.nonEmptyString(event.user_properties)) {
+            event.user_properties = JSON.parse(event.user_properties)
+          }
+        }
+      }
+
+      if (! isEventOk(event)) {
+        logger.warn({ type: 'event.malformed', event }, 'Dropping malformed event')
+        message.ack()
+        return null
+      }
+
+      if (event.user_id) {
+        event.user_id = hash(event.user_id)
+      }
+    
+      event.insert_id = hash(event.user_id, event.device_id, event.session_id, event.event_type, event.time)
+      
+      if (IDENTIFY_VERBS.some(verb => is.assigned(event.user_properties[verb]))) {
+        identify_events.push({
+          device_id: event.device_id,
+          user_id: event.user_id,
+          user_properties: splitIdentifyPayload(event.user_properties),
+        })
+      }
+
+      return event
+    })
+
+    // Filter out exact duplicate identify events to help prevent rate limiting
+    identify_events = identify_events.filter((outer_event, index, self) => {
+      return index === self.findIndex((inner_event) => {
+        return equal(outer_event, inner_event)
+      })
+    })
+
+    // Filter out duplicate user ids in identify events to help prevent rate limiting
+    identify_events = identify_events.filter((outer_event, index, self) => {
+      return index === self.findIndex((inner_event) => {
+        return outer_event.user_id === inner_event.user_id
+      })
+    })
+
+    if (identify_events.length > 0) {
+      let identifying_response = await retry (async bail => {
+        let identifying_response = await request('https://api.amplitude.com/identify', {
+          method: 'POST',
+          lookup,
+          formData: {
+            api_key: AMPLITUDE_API_KEY,
+            identification: JSON.stringify(identify_events)
+          },
+          timeout: 5 * 1000
+        })
+
+        return identifying_response
+      }, {
+        minTimeout: 15000,
+        maxTimeout: 60000,
+        onRetry: error => {
+          logger.error({type: 'identify.error', error: error})
+        }
+      })
+      
+      if (identifying_response == 'success') {
+        logger.info({type: 'identify.success', count: identify_events.length}, 'Identify success')
+      } else {
+        throw new Error('Something goofed in the Identify API')
+      }
     }
-    timeout = setTimeout(onTimeout, TIMEOUT_THRESHOLD);
 
-    processMessage(cargo, message)
-  })
+    let http_response = await retry (async bail => {
+      let http_response = await request('https://api.amplitude.com/2/httpapi', {
+        method: 'POST',
+        lookup,
+        json: true,
+        body: {
+          api_key: AMPLITUDE_API_KEY,
+          events: http_events
+        },
+        timeout: 5 * 1000
+      }, {
+        onRetry: error => {
+          logger.error({type: 'http.error', error: error})
+        }
+      })
 
-  subscription.on('error', error => {
-    logger.error({ type: 'subscription.error', error }, 'Subscription error')
-  })
+      return http_response
+    })
 
-  subscription.on('close', () => {
-    logger.fatal({ type: 'subscription.close' }, 'Subscription closed')
-    process.exit(1)
-  })
-}
-
-function setupCargo (endpoint, key) {
-  const cargo = async.cargo(async payload => {
-    try {
-      await sendPayload(payload, endpoint, key)
-      clearMessages(payload, message => message.ack())
-      logger.info({ type: 'events.processed', endpoint, count: payload.length }, 'Events processed')
-    } catch (error) {
-      logger.error({ type: 'events.error', endpoint, error, count: payload.length }, 'Events error')
-      // Smear nacks over 5 minute period
-      clearMessages(payload, message => message.nack(60 + (Math.random() * 240)), true)
+    if (http_response.code == 200) {
+      logger.info({type: 'http.success', count: http_events.length}, 'HTTP success')
+    } else {
+      logger.info(http_response)
+      throw new Error('Something goofed in the HTTP API')
     }
+
+    payload.forEach(message => message.ack())
   }, MAX_EVENTS_PER_BATCH)
 
-  cargo.concurrency = WORKER_COUNT
-
-  return cargo
+  setTimeout(() => {
+    subscription.close();
+  }, 60000);
 }
 
-function processMessage (cargo, message) {
-  const { httpapi, identify } = parseMessage(message)
-
-  if (message.publishTime < Date.now() - WARNING_THRESHOLD) {
-    logger.warn({ type: 'events.old', httpapi, identify, pubsub_message: { publishTime: message.publishTime }}, 'Old message')
-  }
-
-  if (httpapi) {
-    MESSAGES.set(httpapi.insert_id, { message, payloadCount: identify ? 2 : 1 })
-    cargo.httpapi.push(httpapi)
-  }
-
-  if (identify) {
-    cargo.identify.push(identify)
-  }
-}
-
-function parseMessage (message) {
-  let { jsonPayload: event } = JSON.parse(Buffer.from(message.data, 'base64').toString())
-
-  if (event.Fields) {
-    event = event.Fields
-
-    if (event.op && event.data) {
-      event = JSON.parse(event.data)
-    } else {
-      if (is.nonEmptyString(event.event_properties)) {
-        event.event_properties = JSON.parse(event.event_properties)
-      }
-
-      if (is.nonEmptyString(event.user_properties)) {
-        event.user_properties = JSON.parse(event.user_properties)
-      }
-    }
-  }
-
-  if (isIgnoredEvent(event)) {
-    return {}
-  }
-
-  if (! isEventOk(event)) {
-    logger.warn({ type: 'event.malformed', event }, 'Skipping malformed event')
-    return {}
-  }
-
-  if (event.user_id) {
-    event.user_id = hash(event.user_id)
-  }
-
-  event.insert_id = hash(event.user_id, event.device_id, event.session_id, event.event_type, event.time)
-
-  let identify
-  if (IDENTIFY_VERBS.some(verb => is.assigned(event.user_properties[verb]))) {
-    identify = {
-      device_id: event.device_id,
-      user_id: event.user_id,
-      user_properties: splitIdentifyPayload(event.user_properties),
-      // _insert_id is only here so we can uniquely identify each payload and
-      // link it back to its message. It's not actually sent to Amplitude.
-      _insert_id: event.insert_id,
-    }
-  }
-
-  return {
-    httpapi: event,
-    identify,
-  }
-}
-
-function isIgnoredEvent (event) {
-  if (! IGNORED_EVENTS.has(event.event_type)) {
-    return false
-  }
-
-  const criteria = IGNORED_EVENTS.get(event.event_type)
-
-  if (Array.isArray(criteria)) {
-    return criteria.some(criterion => deepMatch(event, criterion))
-  }
-
-  return deepMatch(event, criteria)
-}
-
-function deepMatch (object, criteria) {
-  if (criteria === undefined) {
-    return true
-  }
-
-  return Object.entries(criteria).every(([ key, value ]) => {
-    if (typeof value === 'object') {
-      return deepMatch(object[key], value)
-    }
-
-    return object[key] === value
-  })
-}
-
-function isEventOk (event) {
-  return (
-    is.nonEmptyString(event.device_id) ||
-    is.nonEmptyString(event.user_id)
-  ) &&
-    is.nonEmptyString(event.event_type) &&
-    is.positive(event.time)
-}
+main()
 
 function hash (...properties) {
   const hmac = crypto.createHmac('sha256', HMAC_KEY)
@@ -269,6 +174,18 @@ function hash (...properties) {
   return hmac.digest('hex')
 }
 
+function isEventOk (event) {
+  return (
+    is.nonEmptyString(event.device_id) ||
+    is.nonEmptyString(event.user_id)
+  ) &&
+    is.nonEmptyString(event.event_type) &&
+    is.positive(event.time)
+}
+
+const IDENTIFY_VERBS = [ '$set', '$setOnce', '$add', '$append', '$unset' ]
+const IDENTIFY_VERBS_SET = new Set(IDENTIFY_VERBS)
+
 function splitIdentifyPayload (properties) {
   return Object.entries(properties).reduce((payload, [ key, value ]) => {
     if (IDENTIFY_VERBS_SET.has(key)) {
@@ -277,46 +194,4 @@ function splitIdentifyPayload (properties) {
     }
     return payload
   }, {})
-}
-
-function sendPayload (payload, endpoint, key) {
-  return request(endpoint, {
-    method: 'POST',
-    lookup,
-    formData: {
-      api_key: AMPLITUDE_API_KEY,
-      [key]: JSON.stringify(payload.map(item => ({ ...item, _insert_id: undefined })))
-    },
-    timeout: 5 * 1000
-  })
-}
-
-function clearMessages (payload, action, forceAction = false) {
-  payload.forEach(event => {
-    // eslint-disable-next-line no-underscore-dangle
-    const id = event.insert_id || event._insert_id
-
-    const item = MESSAGES.get(id)
-    if (! item) {
-      // In this case the message has already been cleared due to an earlier failure
-      return
-    }
-
-    const { message, payloadCount } = item
-    if (! message) {
-      return
-    }
-
-    if (forceAction || payloadCount === 1) {
-      action(message)
-      MESSAGES.delete(id)
-    } else {
-      MESSAGES.set(id, { message, payloadCount: payloadCount - 1 })
-    }
-  })
-}
-
-function onTimeout () {
-  logger.fatal({ type: 'process.timeout' }, `No messages received in ${TIMEOUT_THRESHOLD / SECOND} seconds`)
-  process.exit(1)
 }
