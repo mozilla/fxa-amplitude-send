6ea19381fe98f0c07f4458e8f33da6e39dc30c08
Jon Buckley <jbuckley@mozilla.com>
2019-11-29 17:50:53 -0500

    Add synchronous pull version

---
+++ pubsub/utils.js
@@ -0,0 +1,108 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, you can obtain one at https://mozilla.org/MPL/2.0/.
+
+'use strict'
+
+const crypto = require('crypto')
+const is = require('check-types')
+const { lookup } = require('lookup-dns-cache')
+const request = require('request-promise')
+
+function hash (HMAC_KEY, ...properties) {
+  const hmac = crypto.createHmac('sha256', HMAC_KEY)
+
+  properties.forEach(property => {
+    if (property) {
+      hmac.update(`${property}`)
+    }
+  })
+
+  return hmac.digest('hex')
+}
+
+function isEventOk (event) {
+  return (
+    is.nonEmptyString(event.device_id) ||
+    is.nonEmptyString(event.user_id)
+  ) &&
+    is.nonEmptyString(event.event_type) &&
+    is.positive(event.time)
+}
+
+function parseMessage (event, HMAC_KEY) {
+  if (event.Fields) {
+    event = event.Fields
+
+    if (event.op && event.data) {
+      event = JSON.parse(event.data)
+    } else {
+      if (is.nonEmptyString(event.event_properties)) {
+        event.event_properties = JSON.parse(event.event_properties)
+      }
+
+      if (is.nonEmptyString(event.user_properties)) {
+        event.user_properties = JSON.parse(event.user_properties)
+      }
+    }
+  }
+
+  if (! isEventOk(event)) {
+    return {}
+  }
+
+  if (event.user_id) {
+    event.user_id = hash(HMAC_KEY, event.user_id)
+  }
+
+  event.insert_id = hash(HMAC_KEY, event.user_id, event.device_id, event.session_id, event.event_type, event.time)
+
+  let identify
+  if (IDENTIFY_VERBS.some(verb => is.assigned(event.user_properties[verb]))) {
+    identify = {
+      device_id: event.device_id,
+      event_type: '$identify',
+      user_id: event.user_id,
+      user_properties: splitIdentifyPayload(event.user_properties),
+    }
+  }
+
+  return {
+    httpapi: event,
+    identify,
+  }
+}
+
+function sendMessages (events, AMPLITUDE_API_KEY) {
+  return request('https://api.amplitude.com/batch', {
+    method: 'POST',
+    lookup,
+    json: true,
+    body: {
+      api_key: AMPLITUDE_API_KEY,
+      events: events
+    },
+    timeout: 5 * 1000
+  })
+}
+
+const IDENTIFY_VERBS = [ '$set', '$setOnce', '$add', '$append', '$unset' ]
+const IDENTIFY_VERBS_SET = new Set(IDENTIFY_VERBS)
+
+function splitIdentifyPayload (properties) {
+  return Object.entries(properties).reduce((payload, [ key, value ]) => {
+    if (IDENTIFY_VERBS_SET.has(key)) {
+      payload[key] = value
+      properties[key] = undefined
+    }
+    return payload
+  }, {})
+}
+
+module.exports = {
+  hash,
+  isEventOk,
+  parseMessage,
+  sendMessages,
+  splitIdentifyPayload,
+}
